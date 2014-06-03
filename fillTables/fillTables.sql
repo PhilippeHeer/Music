5d4e952051cd52bdcbcb8b934079256f54a1c4ad
PhilippeHeer <philippe.heer@epfl.ch>
2014-06-03 23:27:01 +0200

    final

--- fillTables/fillTables.sql
+++ fillTables/fillTables.sql
@@ -1,26 +1,26 @@
 /* Type */
 INSERT INTO Type (Type_id, Name) VALUES (0, 'Person')
-/INSERT INTO Type (Type_id, Name) VALUES (1, 'Groupe')
+INSERT INTO Type (Type_id, Name) VALUES (1, 'Groupe')
 
 /* Gender */
-/INSERT INTO Gender (Gender_id, Name) VALUES (0, 'Male')
-/INSERT INTO Gender (Gender_id, Name) VALUES (1, 'Female')
-/INSERT INTO Gender (Gender_id, Name) VALUES (2, 'Ohter')
+INSERT INTO Gender (Gender_id, Name) VALUES (0, 'Male')
+INSERT INTO Gender (Gender_id, Name) VALUES (1, 'Female')
+INSERT INTO Gender (Gender_id, Name) VALUES (2, 'Ohter')
 
 /* Genre */
-/INSERT INTO Genre (Genre_id, Name, Count) SELECT * FROM Genre_csv
+INSERT INTO Genre (Genre_id, Name, Count) SELECT * FROM Genre_csv
 
 /* Area */        
-/INSERT INTO Area (Area_id, Name, Type_of_area) SELECT * FROM Area_csv
+INSERT INTO Area (Area_id, Name, Type_of_area) SELECT * FROM Area_csv
 
 /* Artist */
-/INSERT INTO Artist (Artist_id, Name, Type_id, Gender_id, Area_id) SELECT * FROM Artist_csv
+INSERT INTO Artist (Artist_id, Name, Type_id, Gender_id, Area_id) SELECT * FROM Artist_csv
 
 /* is_genre */
-/INSERT INTO is_genre (Artist_id, Genre_id) SELECT *  FROM Artist_genre_csv
+INSERT INTO is_genre (Artist_id, Genre_id) SELECT *  FROM Artist_genre_csv
 
 /* Recording */
-/INSERT INTO Recording (Recording_id, Name, Length) SELECT * FROM Recording_csv
+INSERT INTO Recording (Recording_id, Name, Length) SELECT * FROM Recording_csv
 
 /* has_recorded */
 INSERT INTO has_recorded (Artist_id, Recording_id) SELECT DISTINCT ArtistID, TrackID FROM artist_track_csv NATURAL JOIN Track_csv
